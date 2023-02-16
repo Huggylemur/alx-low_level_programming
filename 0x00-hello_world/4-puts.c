63dd6ac3a3a92276854007ed2ce7af1a289adf0f
Huggylemur <ochiengmercy99@gmail.com>
2023-02-16 06:30:16 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0.
  */
-
 int main(void)
-
 {
-       	puts("\"Programming is like building a multilingual puzzle");
-       	return (0);
+	puts("\"programming is like building a multilingual puzzle")
+        return (0);
 }
+
