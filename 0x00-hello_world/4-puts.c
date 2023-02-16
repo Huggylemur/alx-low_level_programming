dd1f6a60fa0a7b034597e77b3d161533a701611a
Huggylemur <ochiengmercy99@gmail.com>
2023-02-16 09:45:55 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 
 /**
- * main - Entry
- *
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline.
  * Return: Always 0.
  */
 
@@ -11,6 +11,5 @@ int main(void)
 
 {
 	puts("\"programming is like building a multilingual puzzle");
-
 	return (0);
 }	
